348cece1210dc519eca81646b227105ba83ceebe
srenghong <srenghong21@gmail.com>
2019-08-18 16:52:02 +0200

    react with semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { Container } from "semantic-ui-react";
+import Login from "./Login";
+import Menu from "./Menu";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    <Menu />
+    <Container>
+      <Login />
+    </Container>
+  </Fragment>
+);
 
 export default App;
